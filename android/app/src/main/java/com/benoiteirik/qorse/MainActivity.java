abe0a25693f8ddb76f638f0a1d55879c62f1f3ec
BenoitEirik <olafsergent@gmail.com>
2022-07-21 17:44:08 +0200

    Add Capacitor base configuration

---
+++ android/app/src/main/java/com/benoiteirik/qorse/MainActivity.java
@@ -0,0 +1,5 @@
+package com.benoiteirik.qorse;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.benoiteirik.qorse',
+  appName: 'Qorse',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
