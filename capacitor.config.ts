579e3bde929d1bd40b2ce4d2abee00dd7ddfecdd
BenoitEirik <olafsergent@gmail.com>
2022-07-24 20:27:28 +0200

    Add signature for APK & Capacitor custom environment

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,29 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
-const config: CapacitorConfig = {
+const baseConfig: CapacitorConfig = {
   appId: 'com.benoiteirik.qorse',
   appName: 'Qorse',
   webDir: 'build',
-  bundledWebRuntime: false,
-  server: {
-    url: "http://192.168.214.117:3000",
-    cleartext: true
-  },
+  bundledWebRuntime: false
 };
+
+var config = {};
+
+switch (process.env.NODE_ENV) {
+  case "production":
+    config = {
+      ...baseConfig
+    };
+    break;
+  default:
+    config = {
+      ...baseConfig,
+      server: {
+        url: "http://192.168.214.117:3000",
+        cleartext: true
+      }
+    };
+    break;
+}
 
 export default config;
